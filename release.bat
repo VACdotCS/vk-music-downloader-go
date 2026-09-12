36c23116a7435bda9be697e1bea5937a095f6455
Vladimir Taburkin <09062004v@gmail.com>
2026-09-12 17:51:25 +0300

    Chore: add release.bat for native Windows builds

---
+++ release.bat
@@ -0,0 +1,18 @@
+@echo off
+chcp 65001 > nul
+echo 🔨 Начинаем сборку релизных версий...
+
+if not exist "release" mkdir release
+
+echo 🪟 Собираем для Windows (amd64)...
+set GOOS=windows
+set GOARCH=amd64
+go build -ldflags="-s -w" -o release\vk-music-downloader-windows-amd64.exe main.go
+
+echo 🐧 Собираем для Linux (amd64)...
+set GOOS=linux
+set GOARCH=amd64
+go build -ldflags="-s -w" -o release\vk-music-downloader-linux-amd64 main.go
+
+echo ✅ Готово! Бинарники лежат в папке release\
+pause
